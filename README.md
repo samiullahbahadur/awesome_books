c641c8542316644edbb9f1d91e7dd2ef2f1da100
samiullahbahadur <bahadursamiullah@gmail.com>
2022-02-15 15:02:36 +0430

    Modify the readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# awesome_books
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Awesome Books
+
+> In this project we are making a library where you can save all your favorite books!
+
+## Built With
+
+- HTML
+- Javascript
+
+## Authors
+
+👤 **Samiullahbahadure**
+
+- GitHub: [@githubhandle](https://github.com/samiullahbahadur)
+
+👤 **LionRouge1**
+
+- GitHub: [@githubhandle](https://github.com/LionRouge1)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- js/main.js
+++ js/main.js
@@ -1,53 +1,55 @@
+function addBook(title, author) {
+  if (
+    localStorage.getItem("books") === null ||
+    JSON.parse(localStorage.getItem("books")).length <= 0
+  ) {
+    const books = [
+      {
+        Id: 1,
+        Btitle: title,
+        Bauthor: author,
+      },
+    ];
+    const storeBook = JSON.stringify(books);
+    localStorage.setItem("books", storeBook);
+  } else {
+    const books = JSON.parse(localStorage.getItem("books"));
 
- function addBook (title, author) {
-    if (localStorage.getItem('books') === null || JSON.parse(localStorage.getItem('books')).length <= 0) {
-      const books = [
-        {
-          Id : 1,
-          Btitle : title,
-          Bauthor : author,
-        }
-      ]
-      const storeBook = JSON.stringify(books);
-      localStorage.setItem('books', storeBook);
-    }else {
-      const books = JSON.parse(localStorage.getItem('books'));
+    let lastId = books[books.length - 1].Id + 1;
+    const book = {
+      Id: lastId,
+      Btitle: title,
+      Bauthor: author,
+    };
 
-      let lastId = books[books.length -1].Id +1;
-      const book = {
-          Id : lastId,
-          Btitle : title,
-          Bauthor : author,
-        }
-
-      books.push(book);
-      const storeBook = JSON.stringify(books);
-      localStorage.setItem('books', storeBook);
-    }
+    books.push(book);
+    const storeBook = JSON.stringify(books);
+    localStorage.setItem("books", storeBook);
   }
+}
 
-  function removeBook (bookId) {
-    const books = JSON.parse(localStorage.getItem('books'));
-    const remove = books.filter(book => book.Id != bookId);
-    const storeBook = JSON.stringify(remove);
-    localStorage.setItem('books', storeBook);
-  }
+function removeBook(bookId) {
+  const books = JSON.parse(localStorage.getItem("books"));
+  const remove = books.filter((book) => book.Id != bookId);
+  const storeBook = JSON.stringify(remove);
+  localStorage.setItem("books", storeBook);
+}
 
 //  display all the books;
 
-function displayBook(){
-  if (localStorage.getItem('books') !== null) { 
-    const books = JSON.parse(localStorage.getItem('books'));
+function displayBook() {
+  if (localStorage.getItem("books") !== null) {
+    const books = JSON.parse(localStorage.getItem("books"));
     for (let i = 0; i < books.length; i++) {
       const Pbook = `
       <div class="book">
-        <div class="title">${books[i].Btitle}</div>
-        <div class="auther">${books[i].Bauthor}</div>
+        <div class="title">  Title: ${books[i].Btitle}</div>
+        <div class="auther"> Author: ${books[i].Bauthor}</div>
         <button type="button" class="btn" id="${books[i].Id}">Remove</button>
         <hr>
       </div>`;
-      const container = document.querySelector('.books_container');
-      container.insertAdjacentHTML('beforeend', Pbook);
+      const container = document.querySelector(".books_container");
+      container.insertAdjacentHTML("beforeend", Pbook);
     }
   }
 }
@@ -56,24 +58,23 @@ displayBook();
 
 // add book function
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
-document.querySelector('#book-form').addEventListener('submit', (e) => { 
-  if (title.value === '' || author.value === '') {
-    alert('Title and Author fields must be filled out');
-    e.preventDefault()
-  }else {
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  if (title.value === "" || author.value === "") {
+    alert("Title and Author fields must be filled out");
+    e.preventDefault();
+  } else {
     addBook(title.value, author.value);
   }
 });
 
-
 // Remove book function
 
-const btn = document.querySelectorAll('.btn');
-btn.forEach(element => {
-  element.addEventListener('click',function(){
+const btn = document.querySelectorAll(".btn");
+btn.forEach((element) => {
+  element.addEventListener("click", function () {
     removeBook(this.id);
     location.reload();
   });
