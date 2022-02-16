6cbda0b9c13f09c74379f34837bb0cb9d3b0db6a
samiullahbahadur <bahadursamiullah@gmail.com>
2022-02-16 16:57:15 +0430

    Add navigation section

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 class Books {
-  constructor(title = '', author = '') {
+  constructor(title = "", author = "") {
     this.title = title;
     this.author = author;
-    this.books = (localStorage.getItem('books') !== null) ? JSON.parse(localStorage.getItem('books')) : '';
+    this.books =
+      localStorage.getItem("books") !== null
+        ? JSON.parse(localStorage.getItem("books"))
+        : "";
   }
 
   saveBook(BTSave = this.books) {
     const storeBook = JSON.stringify(BTSave);
-    localStorage.setItem('books', storeBook);
+    localStorage.setItem("books", storeBook);
   }
 
   addBook() {
-    if (this.books === '' || this.books.length <= 0) {
+    if (this.books === "" || this.books.length <= 0) {
       this.books = [
         {
           Id: 1,
@@ -38,8 +41,8 @@ class Books {
   }
 
   static displayBook(book) {
-    const list = document.querySelector('.books_table');
-    const row = document.createElement('tr');
+    const list = document.querySelector(".books_table");
+    const row = document.createElement("tr");
     row.innerHTML = ` <td>${book.Btitle}</td> 
     <td>${book.Bauthor}</td> <td>
     <button type="submit" id="${book.Id}" class="btn">Remove</button></td> `;
@@ -49,8 +52,8 @@ class Books {
 
 //  display all the books;
 
-if (localStorage.getItem('books') !== null) {
-  const books = JSON.parse(localStorage.getItem('books'));
+if (localStorage.getItem("books") !== null) {
+  const books = JSON.parse(localStorage.getItem("books"));
   books.forEach((element) => {
     Books.displayBook(element);
   });
@@ -58,14 +61,14 @@ if (localStorage.getItem('books') !== null) {
 
 // add book function
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-  if (title.value === '' || author.value === '') {
-    const error = document.getElementById('error');
-    error.textContent = 'Title and Author fields must be filled out';
-    error.style.color = 'red';
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  if (title.value === "" || author.value === "") {
+    const error = document.getElementById("error");
+    error.textContent = "Title and Author fields must be filled out";
+    error.style.color = "red";
     e.preventDefault();
   } else {
     const addBk = new Books(title.value, author.value);
@@ -81,7 +84,34 @@ function removeBk() {
   this.parentNode.parentNode.remove();
 }
 
-const btn = document.querySelectorAll('.btn');
+const btn = document.querySelectorAll(".btn");
 btn.forEach((element) => {
-  element.addEventListener('click', removeBk);
+  element.addEventListener("click", removeBk);
+});
+
+/* navbar section */
+const listLink = document.getElementById("list-link");
+const addBook = document.getElementById("add-link");
+const contantPage = document.getElementById("contact-link");
+
+const sectionList = document.getElementById("list");
+const sectionAdd = document.getElementById("add_book");
+const sectionContact = document.getElementById("contact");
+
+listLink.addEventListener("click", () => {
+  sectionList.style.display = "block";
+  sectionAdd.style.display = "none";
+  sectionContact.style.visibility = "hidden";
+});
+
+addBook.addEventListener("click", () => {
+  sectionAdd.style.display = "block";
+  sectionList.style.display = "none";
+  sectionContact.style.visibility = "hidden";
+});
+
+contantPage.addEventListener("click", () => {
+  sectionContact.style.visibility = "visible";
+  sectionAdd.style.display = "none";
+  sectionList.style.display = "none";
 });
