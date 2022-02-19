55c677262956c6d255ed2401c5af0b5a7a4a4f91
samiullahbahadur <bahadursamiullah@gmail.com>
2022-02-19 21:18:29 +0430

    Add danamically  flash message section

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 class Books {
-  constructor(title = '', author = '') {
+  constructor(title = "", author = "") {
     this.title = title;
     this.author = author;
-    this.books = (localStorage.getItem('books') !== null) ? JSON.parse(localStorage.getItem('books')) : '';
+    this.books =
+      localStorage.getItem("books") !== null
+        ? JSON.parse(localStorage.getItem("books"))
+        : "";
   }
 
   saveBook(BTSave = this.books) {
     const storeBook = JSON.stringify(BTSave);
-    localStorage.setItem('books', storeBook);
+    localStorage.setItem("books", storeBook);
   }
 
   addBook() {
-    if (this.books === '' || this.books.length <= 0) {
+    if (this.books === "" || this.books.length <= 0) {
       this.books = [
         {
           Id: 1,
@@ -38,8 +41,8 @@ class Books {
   }
 
   static displayBook(book) {
-    const list = document.querySelector('.books_table');
-    const row = document.createElement('tr');
+    const list = document.querySelector(".books_table");
+    const row = document.createElement("tr");
     row.innerHTML = ` <td>${book.Btitle}</td> 
     <td>${book.Bauthor}</td> <td>
     <button type="submit" id="${book.Id}" class="btn">Remove</button></td> `;
@@ -47,29 +50,55 @@ class Books {
   }
 }
 
+/* flash messages */
+const snakbar = document.getElementById("message-container");
+function snaker(type, mes, time) {
+  const para = document.createElement("p");
+  para.classList.add("snakbar");
+  para.innerHTML = `${mes} `;
+  if (type === "error") {
+    para.classList.add("error");
+  } else if (type === "success") {
+    para.classList.add("success");
+  } else if (type === "remove") {
+    para.classList.add("remove");
+  }
+  snakbar.appendChild(para);
+  para.classList.add("fadeout");
+  setTimeout(() => {
+    snakbar.removeChild(para);
+  }, time);
+}
+
 //  display all the books;
 
-if (localStorage.getItem('books') !== null) {
-  const books = JSON.parse(localStorage.getItem('books'));
+if (localStorage.getItem("books") !== null) {
+  const books = JSON.parse(localStorage.getItem("books"));
   books.forEach((element) => {
     Books.displayBook(element);
   });
 }
 
 // add book function
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  if (title.value === "" || author.value === "") {
+    snaker(
+      "error",
+      "<b>Error:</b> Title and Author fields must be filled out",
+      4000
+    );
 
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-  if (title.value === '' || author.value === '') {
-    const error = document.getElementById('error');
-    error.textContent = 'Title and Author fields must be filled out';
-    error.style.color = 'red';
     e.preventDefault();
   } else {
     const addBk = new Books(title.value, author.value);
     addBk.addBook();
+    snaker("success", "<b> Success: </b> Book Saved successfully!", 4000);
+
+    title.value = "";
+    author.value = "";
   }
 });
 
@@ -79,9 +108,54 @@ function removeBk() {
   const Book = new Books();
   Book.removeBook(this.id);
   this.parentNode.parentNode.remove();
+  snaker("remove", "<b> Remove:</b> Book Removed successfully ", 4000);
 }
 
-const btn = document.querySelectorAll('.btn');
+const btn = document.querySelectorAll(".btn");
 btn.forEach((element) => {
-  element.addEventListener('click', removeBk);
+  element.addEventListener("click", removeBk);
+});
+
+// navbar section
+
+const links = document.querySelectorAll(".links");
+
+function showBlock(e) {
+  const sectionList = document.getElementById("list");
+  const sectionAdd = document.getElementById("add_book");
+  const sectionContact = document.getElementById("contact");
+  switch (e) {
+    case "list-link":
+      sectionList.style.display = "block";
+      sectionAdd.style.display = "none";
+      sectionContact.style.display = "none";
+      break;
+
+    case "add-link":
+      sectionAdd.style.display = "block";
+      sectionList.style.display = "none";
+      sectionContact.style.display = "none";
+      break;
+
+    case "contact-link":
+      sectionContact.style.display = "flex";
+      sectionAdd.style.display = "none";
+      sectionList.style.display = "none";
+      break;
+
+    default:
+      sectionList.style.display = "block";
+      sectionAdd.style.display = "none";
+      sectionContact.style.display = "none";
+      break;
+  }
+}
+
+links.forEach((element) => {
+  element.addEventListener("click", function () {
+    showBlock(element.id);
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 });
